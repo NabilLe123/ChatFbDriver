51e80aeb61b71a744e366b2fd0996aaf4c2e74d6
s.nabil <s.nabil@lesoft.com>
2020-05-13 10:28:45 +0530

    initial commit

---
+++ app/src/main/java/com/lelab/chatfbdriver/MainActivity.java
@@ -0,0 +1,33 @@
+package com.lelab.chatfbdriver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText etLogin = findViewById(R.id.et_login);
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etLogin.getText().toString().equals("driver123")) {
+                    Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                    intent.putExtra("driver", etLogin.getText().toString().trim());
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
